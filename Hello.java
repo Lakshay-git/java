e933ce97ab13f6348aa863af53187a5ff616d9b5
Lakshay soni <lakshaysoni5252@gmail.com>
2024-01-13 23:25:41 +0530

    ..

--- Hello.java
+++ Hello.java
@@ -1,6 +1,19 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Lakshay");
     }
 }
 
+//note1 inside the source file if your class is declared as public it means it must save it from class name .java 
+
+// note2 if inside the public class there is no main method then the source file we have only public class then during the compilation .file will generated
+
+//components of class 
+// 1 variables 
+// . local variables 
+// . instant variables 
+// . static variables 
+// 2 methods 
+// 3 constructor 
+// 4 instant block 
+// 5 static block 
