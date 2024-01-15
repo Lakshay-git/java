020b619266588ab6573f6ffdc4cedcb6fd829f3e
Lakshay soni <lakshaysoni5252@gmail.com>
2024-01-15 23:04:58 +0530

    java

---
+++ Education.java
@@ -0,0 +1,19 @@
+public class Education {
+    void m1(int a,int b)
+    {
+        System.out.println("m1 method");
+        System.out.println(a);
+    }
+    static void m2(String str)
+    {
+        System.out.println(str);
+    }
+public static void main(String[] arg)
+{
+    Education e = new Education();
+    e.m1(10,20);
+    Education.m2("Rohit");
+    
+}
+
+}
--- test.java
+++ test.java
@@ -1,3 +1,34 @@
-public class test {
-    
+// public class Test{
+//     void m1(){
+//         System.out.println("m1 method");
+
+//     }
+//     static void m2(){
+//         System.out.println("m2 method");
+//     }
+
+//     public static void main(String[] args){
+//         Test t = new Test();
+//         t.m1();
+//         Test.m2();
+//     }
+// }
+
+class Test
+{
+    void m1(int a,int b)
+    {
+        System.out.println(a+b);
+    }
+    int m1(int a,int b)
+    {
+        System.out.println(a+b);
+    }
+    public static voidmain(String[] arg)
+    {
+        Test t = new Test();
+        t.m1(100,200);
+    }
 }
+
+// inside a class it is not possible to create new method with same method signature 
