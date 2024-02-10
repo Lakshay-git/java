b98d53a15cd1821eced965e7cbc82143a6b98240
Lakshay soni <lakshaysoni5252@gmail.com>
2024-02-10 21:54:18 +0530

    promises

---
+++ X1.java
@@ -0,0 +1,43 @@
+public class X1 {
+    
+}
+// class X {
+//     int a=100,b=200;
+//     static int x=500,y=60;
+//     public static void main(String[] args){
+//         X t = new X();
+//         t.add(7,5);
+//     }
+    // void add(int a,int b)
+    // {
+    //     System.out.println(a+b);
+           
+    //         System.out.println(this.a+this.b);
+    //     }
+    // }
+
+    // when there is ambiguity b/w instance variable and local variavble name 
+    //use this keyword to access the inside a method to access instance variable 
+
+    //this keyword will not work if the method is define as the static method 
+
+    //it is always good to define local and instance variable with different name 
+
+
+
+    class X {
+        void m1(int a)
+        {
+            System.out.println(a);
+        }
+        void m1(int a,int b)
+        {
+            System.out.println(a+b);
+        }
+        public static void main(String[] args)
+        {
+            X t= new X();
+            t.m1(10);
+            t.m1(10,20);
+        }
+    }
